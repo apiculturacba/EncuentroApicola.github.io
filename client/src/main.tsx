af081d4eb2709b77724a8883e43d4e242be8bfd8
apiculturacba <santidellarossa@gmail.com>
2025-06-24 15:23:26 -0300

    Update main.tsx
    f3

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { Router } from "wouter";
-
-createRoot(document.getElementById("root")!).render(
-  <Router>
-    <App />
-  </Router>
-);
+createRoot(document.getElementById("root")!).render(<App />);

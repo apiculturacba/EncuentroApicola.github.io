53cd609a7cbbc3a46560a3c2a6adb9a306673dc0
apiculturacba <santidellarossa@gmail.com>
2025-06-24 14:57:47 -0300

    Update main.tsx
    sdasd

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Este es el router especial para GitHub Pages
+import { Router } from "wouter";
+import { createHashHistory } from "wouter/history";
+
+// Le decimos a Wouter que use el "modo seguro"
+const hashHistory = createHashHistory();
+
+// Así se inicia tu app correctamente
+createRoot(document.getElementById("root")!).render(
+  <Router history={hashHistory}>
+    <App />
+  </Router>
+);

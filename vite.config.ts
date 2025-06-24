19f396a2a51e3587c4b4096ccf07078c91b2c9af
apiculturacba <santidellarossa@gmail.com>
2025-06-24 15:28:23 -0300

    Update vite.config.ts
    fwerf

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-export default async () => {
-  const cartographerPlugin =
-    process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined
-      ? [(await import("@replit/vite-plugin-cartographer")).cartographer()]
-      : [];
-
-  return defineConfig({
-    base: "/EncuentroApicola.github.io/",
-    plugins: [react(), runtimeErrorOverlay(), ...cartographerPlugin],
-    resolve: {
-      alias: {
-        "@": path.resolve(import.meta.dirname, "client", "src"),
-        "@shared": path.resolve(import.meta.dirname, "shared"),
-        "@assets": path.resolve(import.meta.dirname, "attached_assets"),
-      },
-    },
-    root: path.resolve(import.meta.dirname, "client"),
-    build: {
-      outDir: path.resolve(import.meta.dirname, "dist/public"),
-      emptyOutDir: true,
-    },
-    server: {
-      fs: {
-        strict: true,
-        deny: ["**/.*"],
-      },
-    },
-  });
-};
+export default defineConfig({
+  base: "/EncuentroApicola.github.io/",
+  plugins: [react()],
+});

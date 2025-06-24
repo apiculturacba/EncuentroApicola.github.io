5bb5f58b62de239896b24781654d0d437f1a9921
apiculturacba <santidellarossa@gmail.com>
2025-06-24 15:32:12 -0300

    Update vite.config.ts
    sdf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   base: "/EncuentroApicola.github.io/",
   plugins: [react()],
+  root: path.resolve(__dirname, "client"),
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
+  },
 });
